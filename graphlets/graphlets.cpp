f0bdf66e32e55af0c800ad778f75fe6456881b7b
sprellas <salma.sohrabi@yahoo.com>
2016-01-08 18:19:39 +0100

    adjacency with on bits

--- graphlets/graphlets.cpp
+++ graphlets/graphlets.cpp
@@ -48,7 +48,139 @@ int main()
 
 	vector<vector<int>> graphlets;
 	graphlets = enumerateSubgraphs(graphVector, delimiterInfo, 3);
+	vector<int> adjacency = adjacencyFromGraphlets(graphVector, delimiterInfo, graphlets);
 
 	return 0;
 }
 
+
+
+const vector<vector<int>> enumerateSubgraphs(vector<int>& graph, vector<int>& delimiter, const int k)
+{
+
+	const int nVertices = delimiter.size() - 1;		//number of vertices in the graph
+	vector<vector<int>> answer;					//stores graphlets info
+
+	for (int i = 0; i < nVertices; i++)
+	{
+		vector<int> extensionV = vertexNeigbors(graph, delimiter, i);	//stores neigbors for each vertex
+		vector<int> subgraphV(1, i);
+		extendSubgraph(subgraphV, extensionV, i, graph, delimiter, k, answer);
+	}
+
+	return answer;
+}
+
+void extendSubgraph(vector<int> subgraphVertices, vector<int> extensionVertices, int v, vector<int>& graph, vector<int>& delimiter, const int k, vector<vector<int>>& answer)
+{
+	if (subgraphVertices.size() == k)
+	{
+		answer.push_back(subgraphVertices);
+		return;
+	}
+
+	int w = 0;
+
+	for (int i = 0; i < extensionVertices.size(); i++)		//remove an arbitrary chosen vertex every time from extensionVertices called w
+	{
+		w = extensionVertices[i];
+		vector<int> extVer;
+		extVer = extensionVertices;
+
+		vector<int> subVer;
+		subVer = subgraphVertices;
+		subVer.push_back(w);
+		extVer.erase(extVer.begin() + i);		//remove w from extVer
+		removeSmallerThanW(extVer, w);			//remove elements in extVer if they are smaller than w (avoid graphlet repitition)
+
+		vector<int> neigbors = findNeighborhood(graph, delimiter, extVer);	//find the neighborhood of the remaining vertices
+
+
+		//add neighboring vertices of w if they are bigger than v and in exclusive neighborhood of other vertices in extensionVertices
+		for (int j = delimiter[w]; j < delimiter[w + 1]; j++)
+		{
+			if ((graph[j] > w) && (!ifContains(neigbors, graph[j])) && (!ifContains(extVer, graph[j])))
+				extVer.push_back(graph[j]);
+		}
+
+		extendSubgraph(subVer, extVer, v, graph, delimiter, k, answer);
+	}
+	return;
+}
+
+//stores neigbors for each vertex
+vector<int> vertexNeigbors(vector<int>& graph, vector<int>& delimiter, int vertex)
+{
+	vector<int> extension;
+	for (int i = delimiter[vertex]; i < delimiter[vertex + 1]; i++)
+	{
+		if (graph[i] > vertex)
+			extension.push_back(graph[i]);
+	}
+	return extension;
+}
+
+//remove elements in inputVector if they are smaller than w 
+void removeSmallerThanW(vector<int>& inputVector, int & w)
+{
+	for (int i = 0; i < inputVector.size(); i++)
+	{
+		if (inputVector[i] < w)
+		{
+			inputVector.erase(inputVector.begin() + i);
+			i--;
+		}
+	}
+}
+
+
+//find the neighborhood of the remaining vertices in ExtensionVertices(EV)
+vector<int> findNeighborhood(vector<int>& graph, vector<int>& delimiter, vector<int>& EV)
+{
+	vector<int> neighborhood;
+
+	for (int i = 0; i < EV.size(); i++)
+		for (int j = delimiter[EV[i]]; j < delimiter[EV[i] + 1]; j++)
+		{
+			neighborhood.push_back(graph[j]);
+		}
+	return neighborhood;
+}
+
+
+//check if input vector contains an element
+bool ifContains(vector<int>& input, int element)
+{
+	return find(input.begin(), input.end(), element) != input.end();
+}
+
+
+vector<int> adjacencyFromGraphlets(vector<int>& graph, vector<int>& delimiter, vector<vector<int>> & graphlets)
+{
+	vector<int> graphletsAdjacency;
+	for (int i = 0; i < graphlets.size(); i++)
+	{
+		int bit = 0;
+		int ifEdge;
+		for (int j = 1; j < graphlets[0].size();j++)
+		{
+			for (int k = 0; k < j; k++)
+			{
+				bit = bit << 1;
+				ifEdge = ifConnected(graphlets[i][j], graphlets[i][k], graph, delimiter);
+				bit = bit | ifEdge;
+			}
+		}
+		graphletsAdjacency.push_back(bit);
+	}
+	return graphletsAdjacency;
+}
+
+//check if two vertices are connected in a graph
+int ifConnected(int vertex1, int vertex2, vector<int>& graph, vector<int>& delimiter)
+{
+	if (find(graph.begin() + delimiter[vertex1], graph.begin() + delimiter[vertex1 + 1] , vertex2) != (graph.begin() + delimiter[vertex1 + 1] ))
+		return 1;
+	return 0;
+
+}
